6d6099ff2838dcc6513d75228601423a54462ee6
Demopan4741 <99538789+Demopan4741@users.noreply.github.com>
2022-05-07 21:55:20 +0900

    [ADD] 플레이어 가드 및 패리 추가, GuardInfo수정
    PlayerGuard.cs 내용 추가, GuardInfo 모호한 변수 수정 및 주석 추가

--- Assets/Scripts/Sevens.Entities/Players/GuardInfo.cs
+++ Assets/Scripts/Sevens.Entities/Players/GuardInfo.cs
@@ -6,11 +6,19 @@ namespace Sevens.Entities.Players
     [Serializable]
     public class GuardInfo
     {
-        public float Duration;
-        public float Stamina;
-        public float StateChangeDelay;
-        public float Cooltime;
-        public float ReduceDamage;
-        public float IncreaseUltGaugeOnParrying;
+        //가드시 스테미나가 데미지를 흡수하는 비율
+        public float StaminaDamageRatio;
+
+        //패링 쿨타임
+        public float parryCooltime;
+
+        //패링 지속시간
+        public float parryableTime;
+
+        //가드시 데미지 감소 비율
+        public float ReduceDamageRatio;
+
+        //패링시 궁극기 게이지 증가량
+        public float IncreaseUltGaugeOnParrying; 
     }
 }
--- Assets/Scripts/Sevens.Entities/Players/PlayerGuard.cs
+++ Assets/Scripts/Sevens.Entities/Players/PlayerGuard.cs
@@ -6,18 +6,76 @@ namespace Sevens.Entities.Players
 {
     public class PlayerGuard : MonoBehaviour 
     {
+        [SerializeField]
+        private GuardInfo _guardInfoComponent;
+        [SerializeField]
+        private Player _playerComponent;
+
+        private float _parryableTimer;
+        private float _parryCooltimer;
+        private bool _isParry;
+
+        private bool _isGuard;
+
+
         // 받을 데미지랑 깎일 스태미너 양?
         public PlayerGuardResult TryGuard(Entity source, float damage) 
         {
             var result = new PlayerGuardResult();
+            if (_isParry)
+            {
+                result.Damage = 0f;
+                result.StaminaDamage = 0f;
+                result.Guarded = true;
+            }
+            else if (_isGuard)
+            {
+                result.Damage = damage * _guardInfoComponent.ReduceDamageRatio;
+                result.StaminaDamage = damage * _guardInfoComponent.StaminaDamageRatio;
+                result.Guarded = true;
+            }
+            else
+            {
+                result.Damage = damage;
+                result.StaminaDamage = 0f;
+                result.Guarded = false;
+            }
             return result;
+        }
+
+        private void Update()
+        {
+            _parryCooltimer += Time.deltaTime;
+
+            if (_isParry)
+            {
+                _parryableTimer += Time.deltaTime;
+                if(_parryableTimer >= _guardInfoComponent.parryableTime)
+                {
+                    _isParry = false;
+                }
+            }
+            if (Input.GetButtonDown("Guard") && _parryCooltimer > _guardInfoComponent.parryCooltime)
+            {
+                _isParry = true;
+                _parryCooltimer = 0f;
+            }
+
+            if (Input.GetButton("Guard"))
+            {
+                _isGuard = true;
+            }
+            else
+            {
+                _isGuard = false;
+            }
         }
     }
 
     public struct PlayerGuardResult
     {
         public float Damage;
-        public float Stamina;
+        public float StaminaDamage;
         public bool Guarded;
     }
 }
