f9635478f9ad0da0d3f9e433d5066b0a20ab4183
Hunyl <hunyoung602@gmail.com>
2022-05-18 22:06:26 +0900

    대화창 UI 제작 중

---
+++ Assets/Scripts/Sevens.UIs/SystemDialogue.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using Sevens.Utils;
+
+namespace Sevens.UIs
+{
+    public class SystemDialogue : MonoBehaviour
+    {
+        private CanvasGroup _canvasGroup;
+
+        [SerializeField]
+        private Text _dialogue;
+
+        [SerializeField]
+        private float _fadeDuration;
+
+        private CoroutineMan _coroutine;
+
+        private void Awake()
+        {
+            _coroutine = new CoroutineMan(this);
+            _canvasGroup = GetComponent<CanvasGroup>();
+            _canvasGroup.alpha = 0f;
+        }
+
+        public void Display(string text)
+        {
+            /*
+             - 기존에 텍스트가 있으면 즉시 없애기
+               - 기존 대기 시간을 즉시 만료 (페이드 아웃 애니메이션 존재)
+               - 그냥 바로 페이드 아웃 없이 삭제 (페이드 아웃 애니메이션 없음)
+             - 새로운 텍스트를 추가
+             - 페이드 인
+            */
+        }
+
+        public void InitiateDialogue(string dialogueText)
+        {
+            _dialogue.text = dialogueText;
+            _canvasGroup.DOFade(1, _fadeDuration);
+        }
+    }
+}
---
+++ Assets/Scripts/Sevens.UIs/TestDialogue.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Sevens.UIs
+{
+    public class TestDialogue : MonoBehaviour
+    {
+        public UnityEvent<string> OnInitiateDialogue;
+        [SerializeField]
+        private string _dialogue = "교수를 교수형에 처한다";
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            OnInitiateDialogue.Invoke(_dialogue);
+        }
+    }
+}
