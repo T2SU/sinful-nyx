3e57bd6dbe337ae6ed7e469ab36b3f0636fa4cae
Hunyl <hunyoung602@gmail.com>
2022-05-20 20:32:00 +0900

    DontDestroyOnLoad가 필요한 오브젝트 스크립트 추가

---
+++ Assets/Scripts/DestroyManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Scripts
+{
+    public class DestroyManager : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject[] _essentialObjects;
+
+        private void Awake()
+        {
+            foreach(var obj in _essentialObjects)
+            {
+                DontDestroyOnLoad(obj);
+            }
+        }
+    }
+}
