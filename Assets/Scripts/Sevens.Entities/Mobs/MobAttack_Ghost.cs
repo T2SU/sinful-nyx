022929dcb2808e204bf87f8f92426bd01bbc6694
hoho-a0372 <69834618+hoho-a0372@users.noreply.github.com>
2022-06-08 21:09:27 +0900

    유령 (근거리) (수정 필요)
    피봇 위치가 실제 유령 위치와 동떨어짐

---
+++ Assets/Scripts/Sevens.Entities/Mobs/MobAttack_Ghost.cs
@@ -0,0 +1,55 @@
+using Sevens.Entities.Players;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+namespace Sevens.Entities.Mobs
+{
+    public class MobAttack_Ghost : MobAttackBase
+    {
+        public GameObject Attack;
+        public GameObject AttackPosition;
+        public float AttackTimeScale;
+
+        [SerializeField]
+        private float _upDuration;
+        public override void Execute(Player player, MobAttackable attackManager)
+        {
+            var key = nameof(MobAttack_bat);
+            attackManager.AttackCoroutines.Register(key, AttackTimeline(attackManager));
+        }
+
+        public override void Cancel(MobAttackable attackManager)
+        {
+            var mob = attackManager.Mob;
+            attackManager.EndAttack(true);
+            ClearObjects();
+        }
+        private IEnumerator AttackTimeline(MobAttackable attackManager)
+        {
+            var mob = attackManager.Mob;
+            var coroutines = attackManager.AttackCoroutines;
+            var player = attackManager.Player;
+            var pos = mob.transform.position;
+
+            mob.PlayAnimation(new AnimationPlayOption("Attack", timeScale: AttackTimeScale), immediatelyTransition: true);
+            var obj = Instantiate(Attack, AttackPosition.transform);
+            _objs.Add(obj);
+            SetAllBlowSourceAs(obj, mob);
+            mob.PlayAudio(nameof(Attack));
+
+            obj.GetComponent<Blow>().OnceOption.Enabled = false;
+
+            yield return new WaitForSeconds(_upDuration);
+
+            obj.GetComponent<Blow>().OnceOption.Enabled = true;
+
+            yield return new WaitForSeconds(AttackTimeScale - _upDuration);
+
+            ClearObjects();
+            attackManager.EndAttack(false);
+        }
+
+    }
+}
