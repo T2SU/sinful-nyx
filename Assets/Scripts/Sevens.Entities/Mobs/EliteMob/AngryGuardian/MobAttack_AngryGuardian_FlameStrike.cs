95b6b994fd0c2c8bad3b2dcae9da2892e8e743b3
Hunyl <hunyoung602@gmail.com>
2022-06-23 01:55:01 +0900

    엘리트 몹 1 이펙트 적용

--- Assets/Scripts/Sevens.Entities/Mobs/EliteMob/AngryGuardian/MobAttack_AngryGuardian_FlameStrike.cs
+++ Assets/Scripts/Sevens.Entities/Mobs/EliteMob/AngryGuardian/MobAttack_AngryGuardian_FlameStrike.cs
@@ -3,6 +3,7 @@ using Sevens.Utils;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 namespace Sevens.Entities.Mobs
 {
@@ -22,20 +23,33 @@ namespace Sevens.Entities.Mobs
         private Transform _AttackPos2;
         [SerializeField]
         private Transform _AttackPos3;
+        [SerializeField]
+        private VisualEffect _chargeEffect;
+        [SerializeField]
+        private GameObject _flameStrikeEffect;
+
         public override IEnumerator Attack(Player player, Mob mob, CoroutineMan coroutines)
         {
+            _chargeEffect.Play();
             mob.PlayAnimation(
                 new AnimationPlayOption("Idle", timeScale: _attackTimeScale),
                 immediatelyTransition: true);
 
             yield return new WaitForSeconds(_chargeTime);
+            _chargeEffect.Stop();
             Instantiate(_flameStrike, _AttackPos1);
+            GameObject obj1 = Instantiate(_flameStrikeEffect, _AttackPos1);
+            Destroy(obj1, 2f);
             mob.PlayAudio("FlameStrike");
             yield return new WaitForSeconds(_flameStrikeDelay);
             Instantiate(_flameStrike, _AttackPos2);
+            GameObject obj2 = Instantiate(_flameStrikeEffect, _AttackPos2);
+            Destroy(obj2, 2f);
             mob.PlayAudio("FlameStrike");
             yield return new WaitForSeconds(_flameStrikeDelay);
             Instantiate(_flameStrike, _AttackPos3);
+            GameObject obj3 = Instantiate(_flameStrikeEffect, _AttackPos3);
+            Destroy(obj3, 2f);
             mob.PlayAudio("FlameStrike");
         }
     }
--- Assets/Scripts/Sevens.Entities/Mobs/EliteMob/AngryGuardian/MobAttack_AngryGuardian_StampAttack.cs
+++ Assets/Scripts/Sevens.Entities/Mobs/EliteMob/AngryGuardian/MobAttack_AngryGuardian_StampAttack.cs
@@ -3,6 +3,7 @@ using Sevens.Utils;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 namespace Sevens.Entities.Mobs
 {
@@ -10,6 +11,8 @@ namespace Sevens.Entities.Mobs
     {
         [SerializeField]
         private GameObject _stampAttack;
+        [SerializeField]
+        private VisualEffect _stampAttackEffect;
         [SerializeField]
         private Transform _attackPos;
         [SerializeField]
@@ -22,6 +25,7 @@ namespace Sevens.Entities.Mobs
                 immediatelyTransition: true);
             yield return new WaitForSeconds(animTime - WarningDuration);
             yield return WarningAction(mob);
+            _stampAttackEffect.Play();
             Instantiate(_stampAttack, _attackPos.position, mob.transform.rotation);
             mob.PlayAudio("StampAttack");
         }
