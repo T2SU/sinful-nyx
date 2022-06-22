7dc27dadb12f679317171bd51cf06b4c555ee6a0
Demopan4741 <99538789+Demopan4741@users.noreply.github.com>
2022-06-23 00:30:49 +0900

    불기둥 패턴 추가
    불기둥 패턴 추가

--- Assets/Scripts/Sevens.Entities/Mobs/EliteMob/AngryGuardian/MobAttack_AngryGuardian_FlameStrike.cs
+++ Assets/Scripts/Sevens.Entities/Mobs/EliteMob/AngryGuardian/MobAttack_AngryGuardian_FlameStrike.cs
@@ -1,18 +1,43 @@
+using Sevens.Entities.Players;
+using Sevens.Utils;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MobAttack_AngryGuardian_FlameStrike : MonoBehaviour
+namespace Sevens.Entities.Mobs
 {
-    // Start is called before the first frame update
-    void Start()
+    public class MobAttack_AngryGuardian_FlameStrike : MobAttackBase
     {
-        
-    }
+        [SerializeField]
+        private GameObject _flameStrike;
+        [SerializeField]
+        private float _attackTimeScale;
+        [SerializeField]
+        private float _chargeTime;
+        [SerializeField]
+        private float _flameStrikeDelay;
+        [SerializeField]
+        private Transform _AttackPos1;
+        [SerializeField]
+        private Transform _AttackPos2;
+        [SerializeField]
+        private Transform _AttackPos3;
+        public override IEnumerator Attack(Player player, Mob mob, CoroutineMan coroutines)
+        {
+            mob.PlayAnimation(
+                new AnimationPlayOption("Idle", timeScale: _attackTimeScale),
+                immediatelyTransition: true);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            yield return new WaitForSeconds(_chargeTime);
+            Instantiate(_flameStrike, _AttackPos1);
+            mob.PlayAudio("FlameStrike");
+            yield return new WaitForSeconds(_flameStrikeDelay);
+            Instantiate(_flameStrike, _AttackPos2);
+            mob.PlayAudio("FlameStrike");
+            yield return new WaitForSeconds(_flameStrikeDelay);
+            Instantiate(_flameStrike, _AttackPos3);
+            mob.PlayAudio("FlameStrike");
+        }
     }
 }
+
